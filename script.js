264295101275cfbbc17da4ff9bd24ce1bf88d1a0
Ileolami <99335756+Ileolami@users.noreply.github.com>
2023-07-08 19:18:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const submitBtn = document.getElementById('submitBtn').addEventListener('click', function (e) {
         const form = document.getElementById('myform');
         const name = document.getElementById('name').value;
-const username = document.getElementById
+const username = document.getElementById('username').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const passwordConfirm = document.getElementById('confirmpassword').value;
 const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-        if (name == "" && email == "" && password == "" && confirmpassword == "") {
+        if (name == "" && email == "" && username =="" && password == "" && confirmpassword == "") {
             alert("Please fill the form");
         }
         else if (name == "") {
             alert("Please enter your name");
         }
+        else if (username == ""){
+         alert("Please enter your username");
+}
         else if (email == "") {
             alert("Please enter your email");
         else if (email.match(validRegex)){
