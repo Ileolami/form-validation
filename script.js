a65cfbd802cb2a011d7fde994f77068480b2e5d5
ileolami <makindemercymiracle@gmail.com>
2023-07-10 13:20:12 +0100

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const submitBtn = document.getElementById('submitBtn').addEventListener('click', function (e) {
         const form = document.getElementById('myform');
         const name = document.getElementById('name').value;
-const username = document.getElementById('username').value;
+        const username = document.getElementById('username').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const passwordConfirm = document.getElementById('confirmpassword').value;
-const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
         if (name == "" && email == "" && username =="" && password == "" && confirmpassword == "") {
             alert("Please fill the form");
